d16afb4ebeaf60d52799726f6d7ddf093da56895
xiaofan0408 <1002758393@qq.com>
2019-08-08 14:24:53 +0800

    Add:  提交代码

---
+++ src/main/java/com/xiaofan0408/leetcode/ValidParentheses.java
@@ -0,0 +1,61 @@
+package com.xiaofan0408.leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * @author xuzefan  2019/8/8 13:44
+ */
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if (s.length() == 0) {
+            return true;
+        }
+        if (s.length()== 1) {
+            return false;
+        }
+        LinkedList<Character> parentheses = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                parentheses.push(ch);
+            }
+
+            switch (ch){
+                case ')':{
+                    Character c = parentheses.poll();
+                    if (c == null || c != '(') {
+                        return false;
+                    }
+                    break;
+                }
+                case ']':{
+                    Character c = parentheses.poll();
+                    if (c == null ||c != '[') {
+                        return false;
+                    }
+                    break;
+                }
+                case '}':{
+                    Character c = parentheses.poll();
+                    if (c == null || c != '{') {
+                        return false;
+                    }
+                    break;
+                }
+                default:{}
+            }
+        }
+        return parentheses.size() == 0;
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses validParentheses = new ValidParentheses();
+        System.out.println(validParentheses.isValid("()"));
+        System.out.println(validParentheses.isValid("()[]{}"));
+        System.out.println(validParentheses.isValid("(]"));
+        System.out.println(validParentheses.isValid("([)]"));
+        System.out.println(validParentheses.isValid("{[]}"));
+        System.out.println(validParentheses.isValid("]}"));
+    }
+}
