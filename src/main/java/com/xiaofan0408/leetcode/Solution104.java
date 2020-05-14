802da01db711ec23892bb478bc3027dca5c08d1b
xiaofan0408 <1002758393@qq.com>
2020-05-14 16:56:27 +0800

    update: Solution104

---
+++ src/main/java/com/xiaofan0408/leetcode/Solution104.java
@@ -0,0 +1,26 @@
+package com.xiaofan0408.leetcode;
+
+/**
+ * @author xuzefan  2020/5/14 16:48
+ */
+public class Solution104 {
+    class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+}
